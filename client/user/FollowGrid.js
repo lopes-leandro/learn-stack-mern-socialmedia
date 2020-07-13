6b60bdb0f497c378475a9372343c8cc6831c0532
Leandro Lopes <lopesleandro.it@outlook.com>
2020-07-13 11:49:59 -0300

    implementado o compomente FollowGrid que permitirá ver a lista de usuários seguidos e seguidores

---
+++ client/user/FollowGrid.js
@@ -0,0 +1,56 @@
+import React from "react";
+import { GridList, GridListTile, Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+const useStyles = makeStyles(theme => ({
+    root: theme.mixins.gutters({
+        paddingTop: theme.spacing(2),
+        display: 'flex',
+        flexWrap: 'wrap',
+        justifyContent: 'space-around',
+        overflow: 'hidden',
+        background: theme.palette.background.paper,
+    }),
+    gridList: {
+        width: 500,
+        height: 220,
+    },
+    titleText: {
+        textAlign: 'center',
+        marginTop: 10
+    },
+    bigAvatar: {
+        width: 60,
+        height: 60,
+        margin: 'auto'
+    }
+}));
+
+export default function FollowGrid(props) {
+    const classes = useStyles();
+
+    return (
+        <div className={classes.root}>
+            <GridList cellHeight={160} className={classes.gridList} cols={4}>
+                {
+                    props.people.map((person, index) => {
+                        return <GridListTile style={{'height':120}} key={index}>
+                            <Link to={"/user/" + person._id}>
+                                <Avatar src={'/api/v1/users/photo' + person._id} 
+                                    className={classes.bigAvatar}/>
+                                <Typography className={classes.titleText}>
+                                    {person.name}
+                                </Typography>
+                            </Link>
+                        </GridListTile>
+                    })
+                }
+            </GridList>
+        </div>
+    )
+}
+FollowGrid.propsType = {
+    people: PropTypes.array.isRequired
+}
